2effd78f5b9c0a3a521d3ef007e1cb11c7ec06d8
BjornFelix <bjorn11.felix@gmail.com>
2021-12-08 11:59:42 +0100

    shooter-assignment-BjornFelix

---
+++ Shooter/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float xSpeed = 10;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+		float xOffset = horizontal * Time.deltaTime * xSpeed;
+		float xNewPos = xOffset + transform.localPosition.x;
+		transform.localPosition = new Vector3(xNewPos, transform.localPosition.y, transform.localPosition.z);
+	}
+}
